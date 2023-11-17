4bd9d5834f407a7b6c1097380da02ac9ca5398ea
fergodie <142615772+fergodie@users.noreply.github.com>
2023-11-17 20:16:44 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["el", "loco", "travieso", "dale", "esto"];
+  let adj = ["ganador", "sueña", "triste", "lugar", "persona"];
+  let pepito = ["pepito", "perro", "pampa", "poco", "mucho"];
+
+  for (let i = 0; i < pronoun.length; ++i) {
+    for (let e = 0; e < adj.length; ++e) {
+      for (let o = 0; o < pepito.length; ++o) {
+        console.log(pronoun[i] + adj[e] + pepito[o] + ".com");
+      }
+    }
+  }
 };
